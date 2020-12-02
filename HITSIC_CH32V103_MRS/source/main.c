6bda6d269f4a4a322c247753604ed36e69904534
Chekhov Mark <maqike@qq.com>
2020-12-02 16:14:28 +0800

    添加版权信息

--- HITSIC_CH32V103_MRS/source/main.c
+++ HITSIC_CH32V103_MRS/source/main.c
@@ -1,18 +1,19 @@
-/********************************** (C) COPYRIGHT *******************************
-* File Name          : main.c
-* Author             : WCH
-* Version            : V1.0.0
-* Date               : 2020/04/30
-* Description        : Main program body.
-*******************************************************************************/
-
-/*
- *@Note
- 串口打印调试例程：
- USART1_Tx(PA9)。
- 本例程演示使用 USART1(PA9) 作打印调试口输出。
-
-*/
+/**
+ * Copyright 2018 - 2020 HITSIC
+ * All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 #include "debug.h"
 
